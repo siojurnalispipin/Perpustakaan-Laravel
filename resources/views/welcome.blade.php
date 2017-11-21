79678640ba3784c4434a0b61cf713c15f6d8ff38
siojurnalispipin <siojurnalispipin@hotmail.com>
2017-11-22 01:27:46 +0700

    ada ubah di judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Perpustakaan
+                    Perpustakaan2
                 </div>
 
                 <div class="links">
