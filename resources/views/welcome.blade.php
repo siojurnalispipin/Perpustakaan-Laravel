e325c05e39a5f7e849fbf4ae44cd6babe6446594
siojurnalispipin <siojurnalispipin@hotmail.com>
2017-11-21 22:04:07 +0700

    Ubah judul dan title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Perpustakaan
                 </div>
 
                 <div class="links">
